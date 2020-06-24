10b2d10c0b44b5386b35b8141ddfe53c4e43916e
kabartolo <adairkate@gmail.com>
2020-06-24 20:54:23 +0800

    Add mdx template component and page creation

---
+++ gatsby-node.js
@@ -0,0 +1,67 @@
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  const parent = getNode(node.parent);
+
+  if (node.internal.type === 'Mdx' && parent.internal.type === 'File') {
+    const path = createFilePath({ node, getNode });
+    const slug = parent.name;
+    const [postType, group] = parent.relativeDirectory.split('/');
+
+    createNodeField({
+      name: 'path',
+      node,
+      value: path,
+    });
+
+    createNodeField({
+      name: 'postType',
+      node,
+      value: postType,
+    });
+
+	  createNodeField({
+		  name: 'slug',
+		  node,
+		  value: slug,
+	  });
+
+    createNodeField({
+      name: 'group',
+      node,
+      value: group,
+    });
+  }
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          id
+          fields {
+            path
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  const posts = result.data.allMdx.nodes;
+
+  posts.forEach(({ node }) => {
+    createPage({
+      path: node.fields.path,
+      component: require.resolve('./src/components/Post'),
+      context: { id: node.id },
+    });
+  });
+}
---
+++ src/components/Post/index.js
@@ -0,0 +1,56 @@
+import React, { useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { MDXProvider } from '@mdx-js/react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { Link } from 'gatsby';
+
+import Code from '../Code';
+import { PostContext } from '../Layout/post-context';
+
+const shortcodes = { Link, Code };
+
+export default function Post({ data: { mdx } }) {
+  const { title } = mdx.frontmatter;
+  const { body, fields } = mdx;
+
+  const { setPostType, setCurrentGroup, setPostID } = useContext(PostContext);
+
+  useEffect(() => {
+    setPostType(fields.postType);
+    setCurrentGroup(fields.group);
+    setPostID(mdx.id);
+  });
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <MDXProvider components={shortcodes}>
+        <MDXRenderer>{body}</MDXRenderer>
+      </MDXProvider>
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      body
+      fields {
+        postType
+        group
+      }
+      frontmatter {
+        title
+      }
+      tableOfContents(maxDepth: 2)
+    }
+  }
+`;
+
+Post.propTypes = {
+  data: PropTypes.shape({
+    mdx: PropTypes.instanceOf(Object).isRequired,
+  }).isRequired,
+};
