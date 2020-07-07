2a4edd0197852a57021888801f0988806b9deeaf
kabartolo <adairkate@gmail.com>
2020-07-08 00:32:28 +0800

    Place CodeBlock line numbers in separate div so they don't scroll

--- src/components/CodeBlock/index.js
+++ src/components/CodeBlock/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import oceanicNext from 'prism-react-renderer/themes/oceanicNext';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
 
 import styles from './codeBlock.module.css';
 
@@ -10,7 +10,7 @@ export default function CodeBlock({ children, metastring, className: gatsbyClass
   const language = gatsbyClassName ? gatsbyClassName.replace(/language-/, '') : '';
   console.log(metastring);
   return (
-    <Highlight {... defaultProps} theme={oceanicNext} code={children} language={language}>
+    <Highlight {... defaultProps} theme={nightOwl} code={children} language={language}>
       {({
         style,
         tokens,
@@ -18,17 +18,27 @@ export default function CodeBlock({ children, metastring, className: gatsbyClass
         getLineProps,
         getTokenProps,
       }) => (
-        <pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div className={styles.line} key={i} {...getLineProps({ line, key: i })}>
-              <span className={styles.number}>{i + 1}</span>
-              <span className={styles.content}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </span>
+        <pre className={`${className} ${styles.prismCode}`} style={style}>
+          <div className={styles.codeContainer}>
+            <div>
+              {tokens.map((line, i) => (
+                <div className={styles.number} key={i}>
+                  {i + 1}
+                </div>
+              ))}
             </div>
-          ))}
+            <div className={styles.code}>
+              {tokens.map((line, i) => (
+                <div className={styles.line} key={i} {...getLineProps({ line, key: i })}>
+                  <span className={styles.content}>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
         </pre>
       )}
     </Highlight>
