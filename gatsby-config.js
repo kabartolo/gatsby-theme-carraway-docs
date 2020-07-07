c914ae535d8223071e1583fa444dfa9e000b80a6
kabartolo <adairkate@gmail.com>
2020-07-08 00:20:46 +0800

    Add SASS, autolink headers and typography.js packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ module.exports = ({ postsPath }) => {
     plugins: [
       'gatsby-plugin-eslint',
       {
+        resolve: 'gatsby-plugin-typography',
+        options: {
+          pathToConfigModule: `${__dirname}/src/utils/typography`,
+        },
+      },
+      {
         resolve: 'gatsby-source-filesystem',
         options: {
           name: 'posts',
@@ -13,7 +19,26 @@ module.exports = ({ postsPath }) => {
         resolve: 'gatsby-plugin-mdx',
         options: {
           extensions: ['.mdx', '.md'],
-        }
+          gatsbyRemarkPlugins: [
+            {
+              resolve: 'gatsby-remark-autolink-headers',
+              options: 
+              { 
+                elements: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+              },
+            }
+          ],
+        },
+      },
+      {
+        resolve: 'gatsby-plugin-sass',
+        options: {
+          data: `@import "${__dirname}/src/styles/global";`,
+          cssLoaderOptions: {
+            camelCase: true,
+          }
+        },
+        includePaths: [`${__dirname}/src/styles/`],
       },
       {
         resolve: 'gatsby-plugin-google-fonts',
