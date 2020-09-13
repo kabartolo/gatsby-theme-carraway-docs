268adf815acd459cdd6179150f675dcfcd2907d9
kabartolo <adairkate@gmail.com>
2020-09-14 02:01:17 +0800

    Remove theme styles from list of Theme-UI components (redundancy)

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -2,17 +2,6 @@
 import { Styled } from 'theme-ui';
 import * as components from '@theme-ui/components';
 
-import theme from './index';
-
-const styles = Object.keys(theme.styles).reduce((map, style) => {
-  if (style === 'pre' || style === 'code') return null;
-  return ({
-    ...map,
-    [style]: Styled[style],
-  });
-}, {});
-
 export default {
-  ...styles,
   ...components,
 };
