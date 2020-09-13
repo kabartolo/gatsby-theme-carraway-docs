4657b9e68b3f2a2b2b4d2c0065c6a4362bab8f19
kabartolo <adairkate@gmail.com>
2020-09-14 01:58:12 +0800

    Add keys to array items in Breadcrumb component

--- src/components/Post/Breadcrumb.js
+++ src/components/Post/Breadcrumb.js
@@ -4,34 +4,36 @@ import { Styled } from 'theme-ui';
 import { Link } from 'gatsby';
 
 export default function Breadcrumb({ data }) {
+  if (!data.length) return null;
+
   const last = data.slice(-1)[0];
   const first = data.length > 1 ? data[0] : null;
   const middle = data.length > 2 ? data.slice(1, -1) : null;
 
-  return data.length && (
+  return (
     <nav>
       {first && (
-        <span>
+        <span key={first.path}>
           <Styled.a as={Link} to={first.path}>
             {first.name}
           </Styled.a>
         </span>
       )}
       {middle && middle.map((section) => (
-        <>
+        <span key={section.path}>
           <span> &#47; </span>
           <span>
             <Styled.a as={Link} to={section.path}>
               {section.name}
             </Styled.a>
           </span>
-        </>
+        </span>
       ))}
       {last && (
-        <>
+        <span key={last.path}>
           {data.length > 1 && <span> &#47; </span>}
           <span>{last.name}</span>
-        </>
+        </span>
       )}
     </nav>
   );
