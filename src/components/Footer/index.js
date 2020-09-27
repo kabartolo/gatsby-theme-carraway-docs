65a79e4a6225b29e03c38b898e178601615184da
kabartolo <adairkate@gmail.com>
2020-09-27 23:47:06 +0800

    Remove user-specified text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
-import { useSiteMetadata } from '../../hooks';
-
 import styles from './footer.module.scss';
 
 export default function Footer() {
-  const { footerText } = useSiteMetadata();
-
   return (
-    <div className={styles.footer}>
-      <p>{footerText}</p>
-    </div>
+    <div className={styles.footer} />
   );
 }
