d85925a255449822dfd8ea6f817352621ff254ad
kabartolo <adairkate@gmail.com>
2020-10-25 22:40:12 +0800

    Change PropertyTable to accept regular Markdown table with specific headers

--- src/components/PropertyTable/index.js
+++ src/components/PropertyTable/index.js
@@ -1,65 +1,93 @@
 /** @jsx jsx */
 /* eslint-disable no-unused-vars */
 import { jsx } from 'theme-ui';
-import React, { useState } from 'react';
+import React, { Children } from 'react';
 import PropTypes from 'prop-types';
+import { useUID } from 'react-uid';
 
 import styles from './property-table.module.scss';
 
-function getProperties(children) {
-  const properties = React.Children.map(children, (child) => {
-    const property = {
-      name: [],
-      type: [],
-      info: [],
-      default: [],
-      description: [],
-    };
-    let prev;
+function getProperties(table) {
+  let properties = [];
 
-    React.Children.forEach(child.props.children, (grandchild) => {
-      if (typeof grandchild === 'string' && grandchild.match(/:/)) {
-        const lines = grandchild.split(/\n/);
-        lines.forEach((line) => {
-          const [key, ...value] = line.split(':');
-          if (Array.isArray(property[key])) {
-            property[key].push(value.join(':'));
-            prev = key;
-          } else {
-            property[prev].push(line);
-          }
-        });
-      } else {
-        property[prev].push(grandchild);
-      }
+  if (table.props.originalType === 'table') {
+    const [thead, tbody] = table.props.children;
+    const columnHeads = thead.props.children.props.children;
+    const rows = tbody.props.children;
+    properties = Children.map(rows, (row) => {
+      const property = {
+        id: useUID(),
+        name: [],
+        type: [],
+        info: [],
+        default: [],
+        description: [],
+      };
+
+      Children.map(row.props.children, (cell, index) => {
+        let key = columnHeads[index].props.children;
+        const value = cell.props.children;
+        if (typeof key === 'string') {
+          key = key.toLowerCase();
+          property[key].push(value);
+        }
+      });
+      return property;
     });
-    return property;
-  });
+  }
 
   return properties;
 }
 
 export default function PropertyTable({ children, title }) {
-  const [properties] = useState(getProperties(children));
-
+  const properties = getProperties(children);
   return (
     <div
       className={`property-table-container ${styles.container}`}
-      sx={{ variant: 'divs.propertyTable' }}
+      sx={{
+        borderTop: '2.5px solid',
+        borderBottom: '2.5px solid',
+        borderColor: 'border',
+      }}
     >
       <h3 className={`property-table-title ${styles.title}`}>{title}</h3>
       <ul className={`property-table-list ${styles.propertyTable}`}>
         {properties.map((property) => (
-          <li key={property.name} className={`property-table-cell ${styles.cell}`}>
+          <li
+            key={property.id}
+            className={`property-table-cell ${styles.cell}`}
+            sx={{
+              borderTop: 'main',
+              borderColor: 'border',
+              variant: 'listItems.plain',
+            }}
+          >
             <div className={`property-top-row ${styles.topRow}`}>
-              <h4 className={`property-name ${styles.name}`}>{property.name}</h4>
-              {property.type && <span className="property-type">{property.type}</span>}
+              <h4
+                className={`property-name ${styles.name}`}
+                sx={{
+                  bg: 'primary',
+                  color: 'background',
+                }}
+              >
+                {property.name}
+              </h4>
+              {property.type && (
+                <span
+                  className={`property-type ${styles.type}`}
+                  sx={{
+                    color: 'grey',
+                  }}
+                >
+                  {property.type}
+                </span>
+              )}
             </div>
             <div className="second-row">
               <span className={`property-info ${styles.info}`}>
                 {property.info}
               </span>
-              {(property.default.length > 0) && (
+              {(property.default.length > 0 && typeof (property.default[0]) !== 'undefined') && (
                 <span className={`property-default-value ${styles.defaultValue}`}>
                   <em>default: </em>
                   {property.default}
@@ -75,10 +103,7 @@ export default function PropertyTable({ children, title }) {
 }
 
 PropertyTable.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]).isRequired,
+  children: PropTypes.node.isRequired,
   title: PropTypes.string,
 };
 
